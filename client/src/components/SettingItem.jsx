02dd929a9c9d994b7f5de970d51329a439789ed0
justin.crawford <jcrawford2000@gmail.com>
2023-08-25 21:55:12 -0700

    Wire up all settings

--- client/src/components/SettingItem.jsx
+++ client/src/components/SettingItem.jsx
@@ -35,6 +35,7 @@ function ToggleItem({setting}){
             setValue("off")
             setChecked(false)
         }
+        setting.onChange(event)
     }
 
     return(
@@ -48,7 +49,7 @@ function ToggleItem({setting}){
                 </div>
             </label>
             <div className="ml-10">
-                <input id={setting.name} type="checkbox"  onClick={onChange} value={value} defaultChecked={value === 'on' ? true : false} className="toggle toggle-info"  />
+                <input id={setting.name} name={setting.name} type="checkbox"  onClick={onChange} value={value} defaultChecked={value === 'on' ? true : false} className="toggle toggle-info"  />
             </div>                    
         </div>
     )
@@ -56,10 +57,10 @@ function ToggleItem({setting}){
 
 function RangeItem({setting}){
     const [value, setValue] = useState(setting.def_val);
-    const [name, setName] = useState(setting.name)
 
     const onChange = (event) => {
         setValue(event.target.value);
+        setting.onChange(event)
     }
 
     return (
@@ -74,7 +75,7 @@ function RangeItem({setting}){
                 
             </label>
             <div className="ml-10 flex">
-                <input name={name} type="range" min={0} max="100" className="range range-info" value={value} onChange={onChange} />
+                <input name={setting.name} id={setting.name} type="range" min={0} max="100" className="range range-info" value={value} onChange={onChange} />
                 <p className="ml-10">{value}</p>
             </div>                    
         </div>
@@ -86,12 +87,16 @@ function TextAreaItem({setting}){
 
     const onChange = (event) => {
         setValue(event.target.value.split('\n'))
-        //setting.onChange(event);
+        console.log(event.target.value.split('\n'))
+        console.log(event.target.type)
+        setting.onChange(event);
     }
 
     const formatTextArea = () => {
         let testText = ''
         value.forEach((element) => { testText += (element + "\n")})
+        //Remove the last \n
+        testText = testText.substring(0,testText.lastIndexOf('\n'))
         return( testText)
     }
 
@@ -106,7 +111,7 @@ function TextAreaItem({setting}){
             </div>                        
         </label>
         <div className="ml-10 block">
-                <textarea id={setting.name} rows={value.length} value={ formatTextArea() } onChange={onChange}  className="textarea textarea-bordered textarea-info w-full max-w-xs" />
+                <textarea id={setting.name} name={setting.name} rows={value.length} value={ formatTextArea() } onChange={onChange}  className="textarea textarea-bordered textarea-info w-full max-w-xs" />
         </div>
     </div> 
     )
@@ -132,7 +137,7 @@ function TextItem({setting}){
                 </div>                        
             </label>
             <div className="ml-10 block">
-                    <input type="text" id={setting.name} value={value} onChange={onChange}  className="input input-bordered input-info w-full max-w-xs" />
+                    <input type="text" name={setting.name} value={value} onChange={onChange}  className="input input-bordered input-info w-full max-w-xs" />
             </div>
         </div>
     )
@@ -144,6 +149,7 @@ function RadioItem({setting}){
     const onChange = (event) => {
         console.log("Change to " + event.target.name + " value=" + event.target.value)
         setValue(event.target.value)
+        setting.onChange(event);
         //event.target.checked = true
     }
     return(
@@ -160,7 +166,7 @@ function RadioItem({setting}){
                 {  setting.options.map((option, index) => {
                     return(
                     <label key={index} className="cursor-pointer">
-                        <input key={index} className="radio radio-info" name={setting.name} type="radio" defaultChecked={option.value === value? true:false} value={option.value} onChange={onChange}/>
+                        <input key={index} className="radio radio-info" name={setting.name} id={setting.name} type="radio" defaultChecked={option.value === value? true:false} value={option.value} onChange={onChange}/>
                         <span className="label-text mr-5 ml-5 align-top">
                             {option.label} 
                         </span>
--- client/src/components/System.jsx
+++ client/src/components/System.jsx
@@ -22,17 +22,31 @@ const System = ({system_id}) => {
      * Finally, call updateSettings from our context to update the global context
      *
     */
-    function onShortNameChange(e) {
-        setSystem({...system, "shortName": e.target.value})
+    function onSettingChange(e) {
+        let targetValue
+        if (e.target.type=='checkbox') {
+            targetValue = e.target.value.checked
+        }
+        else if (e.target.type=='textarea') {
+            targetValue = e.target.value.split('\n').map(el => {
+                let n = Number(el)
+                return isNaN(n) ? el : n
+            })
+            console.log(targetValue)
+        }
+        else {
+            targetValue = e.target.value
+        }
         const newSystems = settings.systems.map(sys => {
             if (sys._id === system._id) {
-                return {...sys, shortName: e.target.value}
+                let updsys = {...sys, [e.target.name]: e.target.value}
+                return (updsys)
             }
             return sys
         })
         updateSettings({...settings, systems:newSystems})
    }
-   console.log("System: " + JSON.stringify(system))
+
    /*
     * Display the settings for this system
     *
@@ -46,7 +60,7 @@ const System = ({system_id}) => {
             label:'Short Name:', 
             name:'shortName', 
             def_val:system.shortName != undefined || system.shortName != null ? system.shortName : '', 
-            onChange: onShortNameChange,
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -56,6 +70,7 @@ const System = ({system_id}) => {
             name:'type', 
             def_val:system.type != undefined || system.type != null ? system.type : '', 
             options: [{label:'SmartNet', value:'smartnet'},{label: 'P25', value:'p25'},{label:'Conventional',value:'conventional'},{label:'Conventional DMR', value:'conventialDMR'},{label:'Conventional P25',value:'conventionalP25'}],
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -64,14 +79,16 @@ const System = ({system_id}) => {
             label:'Control Channels:', 
             name:'control_channels', 
             def_val:system.control_channels != undefined || system.control_channels != null ? system.control_channels : '', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
         {
-            type:'text', 
+            type:'textarea', 
             label:'Channels:', 
             name:'channels', 
             def_val:system.channels != undefined || system.channels != null ? system.channels : '', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -80,6 +97,7 @@ const System = ({system_id}) => {
             label:'Channel File:', 
             name:'channelFile', 
             def_val:system.channelFile != undefined || system.channelFile != null ? system.channelFile : '', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -89,6 +107,7 @@ const System = ({system_id}) => {
             name:'modulation', 
             def_val:system.modulation != undefined || system.modulation != null ? system.modulation : 'qpsk',
             options: [{label:'QPSK',value:'qpsk'},{label:'FSK4', value:'fsk4'}], 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -97,6 +116,7 @@ const System = ({system_id}) => {
             label:'Squelch:', 
             name:'squelch', 
             def_val:system.squelch != undefined || system.squelch != null ? system.squelch : '-160', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -105,6 +125,7 @@ const System = ({system_id}) => {
             label:'Talkgroups File:', 
             name:'talkgroupsFile', 
             def_val:system.talkgroupsFile != undefined || system.talkgroupsFile != null ? system.talkgroupsFile : '', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -113,6 +134,7 @@ const System = ({system_id}) => {
             label:'OpenMHZ API Key:', 
             name:'apiKey', 
             def_val:system.apiKey != undefined || system.apiKey != null ? system.apiKey : '', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -121,6 +143,7 @@ const System = ({system_id}) => {
             label:'Broadcastify API Key:', 
             name:'broadcastifyApiKey', 
             def_val:system.broadcastifyApiKey != undefined || system.broadcastifyApiKey != null ? system.broadcastifyApiKey : '', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -129,6 +152,7 @@ const System = ({system_id}) => {
             label:'Broadcastify System ID:', 
             name:'broadcastifySystemId', 
             def_val:system.broadcastifySystemId != undefined || system.broadcastifySystemId != null ? system.broadcastifySystemId : '', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -137,6 +161,7 @@ const System = ({system_id}) => {
             label:'Upload Script:', 
             name:'uploadScript', 
             def_val:system.uploadScript != undefined || system.uploadScript != null ? system.uploadScript : '', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -145,6 +170,7 @@ const System = ({system_id}) => {
             label:'Compress Wav:', 
             name:'compressWav', 
             def_val:system.compressWav != undefined || system.compressWav != null ? (system.compressWav ? 'on':'off') : 'on', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -153,6 +179,7 @@ const System = ({system_id}) => {
             label:'Unit Script:', 
             name:'unitScript', 
             def_val:system.unitScript != undefined || system.unitScript != null ? system.unitScript : '', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -161,6 +188,7 @@ const System = ({system_id}) => {
             label:'Audio Archive:', 
             name:'audioArchive', 
             def_val:system.audioArchive != undefined || system.audioArchive != null ? (system.audioArchive ? 'on' : 'off') : 'on', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -169,6 +197,7 @@ const System = ({system_id}) => {
             label:'Transmission Archive:', 
             name:'transmissionArchive', 
             def_val:system.transmissionArchive != undefined || system.transmissionArchive != null ? (system.transmissionArchive ? 'on':'off' ): 'off', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -177,6 +206,7 @@ const System = ({system_id}) => {
             label:'Call Log:', 
             name:'callLog', 
             def_val:system.callLog != undefined || system.callLog != null ? (system.callLog ? 'on':'off') : 'on', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -187,6 +217,7 @@ const System = ({system_id}) => {
             def_val:system.analogLevels != undefined || system.analogLevels != null ? system.analogLevels : '8', 
             min: 1,
             max: 32,
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -195,6 +226,7 @@ const System = ({system_id}) => {
             label:'Maximum Deviation:', 
             name:'maxDev', 
             def_val:system.maxDev != undefined || system.maxDev != null ? system.maxDev : '4000', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -205,6 +237,7 @@ const System = ({system_id}) => {
             def_val:system.digitalLevels != undefined || system.digitalLevels != null ? system.digitalLevels : '1', 
             min: 1,
             max: 15,
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -213,6 +246,7 @@ const System = ({system_id}) => {
             label:'Unit Tags File:', 
             name:'unitTagsFile', 
             def_val:system.unitTagsFile != undefined || system.unitTagsFile != null ? system.unitTagsFile : '', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -221,6 +255,7 @@ const System = ({system_id}) => {
             label:'Record Unknown:', 
             name:'recordUnknown', 
             def_val:system.recordUnknown != undefined || system.recordUnknown != null ? (system.recordUnknown ? 'on':'off') : 'on', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -229,6 +264,7 @@ const System = ({system_id}) => {
             label:'Record Unit to Unit Calls:', 
             name:'recordUUVCalls', 
             def_val:system.recordUUVCalls != undefined || system.recordUUVCalls != null ? (system.recordUUVCalls ? 'on':'off') : 'on', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -237,6 +273,7 @@ const System = ({system_id}) => {
             label:'Hide Encrypted:', 
             name:'hideEncrypted', 
             def_val:system.hideEncrypted != undefined || system.hideEncrypted != null ? (system.hideEncrypted ? 'on':'off') : 'off', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -245,6 +282,7 @@ const System = ({system_id}) => {
             label:'Hide Unknown Talkgroups:', 
             name:'hideUnknownTalkgroups', 
             def_val:system.hideUnknownTalkgroups != undefined || system.hideUnknownTalkgroups != null ? (system.hideUnknownTalkgroups ? 'on':'off') : 'off', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -253,6 +291,7 @@ const System = ({system_id}) => {
             label:'Minimum Call Duraction:', 
             name:'minDuration', 
             def_val:system.minDuration != undefined || system.minDuration != null ? system.minDuration : '0', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -261,6 +300,7 @@ const System = ({system_id}) => {
             label:'Minimum Call Duraction:', 
             name:'minDuration', 
             def_val:system.minDuration != undefined || system.minDuration != null ? system.minDuration : '0', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -269,6 +309,7 @@ const System = ({system_id}) => {
             label:'Minimum Transmission Duraction:', 
             name:'minTransmissionDuration', 
             def_val:system.minTransmissionDuration != undefined || system.minTransmissionDuration != null ? system.minTransmissionDuration : '0', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -277,6 +318,7 @@ const System = ({system_id}) => {
             label:'Maximum Call Duraction:', 
             name:'maxDuration', 
             def_val:system.maxDuration != undefined || system.maxDuration != null ? system.maxDuration : '0', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -286,6 +328,7 @@ const System = ({system_id}) => {
             name:'talkgroupDisplayFormat', 
             def_val:system.talkgroupDisplayFormat != undefined || system.talkgroupDisplayFormat != null ? system.talkgroupDisplayFormat : 'id', 
             options:[{label:'ID',value:'id'},{label:'ID-Tag', value:'id_tag'},{label:'Tag-ID', value:'tag_id'}],
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -295,6 +338,7 @@ const System = ({system_id}) => {
             name:'bandplan', 
             def_val:system.bandplan != undefined || system.bandplan != null ? system.bandplan : '800_standard', 
             options:[{label:'800 Standard',value:'800_standard'},{label:'800 Reband',value:'800_reband'},{label:'800 Splinter',value:'800_splinter'},{label:'400 Custom',value:'400_custom'}],
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -303,6 +347,7 @@ const System = ({system_id}) => {
             label:'Band Plan Base:', 
             name:'bandplanBase', 
             def_val:system.bandplanBase != undefined || system.bandplanBase != null ? system.bandplanBase : '', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -311,6 +356,7 @@ const System = ({system_id}) => {
             label:'Band Plan High:', 
             name:'bandplanHigh', 
             def_val:system.bandplanHigh != undefined || system.bandplanHigh != null ? system.bandplanHigh : '', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -319,6 +365,7 @@ const System = ({system_id}) => {
             label:'Band Plan Spacing:', 
             name:'bandplanSpacing', 
             def_val:system.bandplanSpacing != undefined || system.bandplanSpacing != null ? system.bandplanSpacing : '', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -327,6 +374,7 @@ const System = ({system_id}) => {
             label:'Band Plan Offset:', 
             name:'bandplanOffset', 
             def_val:system.bandplanOffset != undefined || system.bandplanOffset != null ? system.bandplanOffset : '', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -335,6 +383,7 @@ const System = ({system_id}) => {
             label:'Decode MDC:', 
             name:'decodeMDC', 
             def_val:system.decodeMDC != undefined || system.decodeMDC != null ? (system.decodeMDC ? 'on':'off') : 'off', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -343,6 +392,7 @@ const System = ({system_id}) => {
             label:'Decode Fleet Sync:', 
             name:'decodeFSync', 
             def_val:system.decodeFSync != undefined || system.decodeFSync != null ? (system.decodeFSync ? 'on':'off') : 'off', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -351,6 +401,7 @@ const System = ({system_id}) => {
             label:'Decode Star:', 
             name:'decodeStar', 
             def_val:system.decodeStar != undefined || system.decodeStar != null ? (system.decodeStar ? 'on':'off') : 'off', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -359,6 +410,7 @@ const System = ({system_id}) => {
             label:'Decode Tactical Public Safety:', 
             name:'decodeTPS', 
             def_val:system.decodeTPS != undefined || system.decodeTPS != null ? (system.decodeTPS ? 'on':'off') : 'off', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         <SettingItem setting={
@@ -367,6 +419,7 @@ const System = ({system_id}) => {
             label:'Enabled:', 
             name:'enabled', 
             def_val:system.enabled != undefined || system.enabled != null ? (system.enabled ? 'on':'off') : 'on', 
+            onChange: onSettingChange,
             tooltip:'tooltip2'
         }}/>
         </form>
